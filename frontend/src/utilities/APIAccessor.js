be9949518a7abc0b0a1c25fccfcf69609bd1b63d
Andrea Pacino <andrea.pacino87@gmail.com>
2018-01-10 13:30:13 +0100

    added few more methods to API accessor

--- frontend/src/utilities/APIAccessor.js
+++ frontend/src/utilities/APIAccessor.js
@@ -27,6 +27,56 @@ export const addPost = (post) =>
       ...headers,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ post })
+    body: JSON.stringify(post)
+  })
+    .then(res => res.json());
+
+export const getPostDetail = (postId) =>
+  fetch(`${baseURL}/posts/${postId}`, { headers })
+    .then(res => res.json())
+    .then(data => data.post);
+
+export const votePost = (post, vote) =>
+  fetch(`${baseURL}/posts/${post.id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ option: vote })
+  })
+    .then(res => res.json());
+
+export const editPost = (post, title, body) =>
+  fetch(`${baseURL}/posts/${post.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({title, body})
+  })
+    .then(res => res.json());
+
+export const deletePost = (post) =>
+  fetch(`${baseURL}/posts/${post.id}`, {
+    method: 'DELETE',
+    headers: headers,
+  })
+    .then(res => res.json());
+
+export const getPostComments = (post) =>
+  fetch(`${baseURL}/posts/${postId}/comments`, { headers })
+    .then(res => res.json())
+    .then(data => data.comments);
+
+export const addComment = (comment) =>
+  fetch(`${baseURL}/comments`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(comment)
   })
     .then(res => res.json());
